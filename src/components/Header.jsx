929019f7b9db96146e00d3bdcfd5dee51fb9d7b4
Adrian Bucur <adrianbucur@macbook-pro.home>
2025-12-17 17:40:08 +0100

    Slightly reduce header logo size

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -56,7 +56,8 @@ function Header() {
             <img
               src="/branding/fulfillment-prep-logo.png"
               alt="Fulfillment Prep Logistics Logo"
-              className="h-12 w-auto object-contain"
+              className="w-auto object-contain"
+              style={{ height: '46px' }}
             />
             <div className="flex flex-col">
               <span className="text-lg font-bold text-text-primary leading-tight">PrepCenter</span>
