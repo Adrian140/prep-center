7cc4aa66868584935d591cb56dc6170fcee65b5b
Adrian Bucur <adrianbucur@macbook-pro.home>
2026-02-09 11:32:09 +0100

    Allow custom service role key for email

--- supabase/functions/fba-step2-confirm-shipping/index.ts
+++ supabase/functions/fba-step2-confirm-shipping/index.ts
@@ -10,6 +10,7 @@ const corsHeaders = {
 
 const SUPABASE_URL = Deno.env.get("SUPABASE_URL") || "";
 const SUPABASE_SERVICE_ROLE_KEY =
+  Deno.env.get("INTERNAL_SERVICE_ROLE_KEY") ||
   Deno.env.get("SUPABASE_SERVICE_ROLE_KEY") ||
   Deno.env.get("SUPABASE_SERVICE_ROLE") ||
   "";
